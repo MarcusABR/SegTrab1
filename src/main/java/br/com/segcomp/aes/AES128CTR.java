50aa7940e4422aa226ffc2194a82e093a67eaa46
João Antonio Desiderio de Moraes <joaoadm94@gmail.com>
2022-09-19 09:32:08 -0300

    ctr mode

---
+++ src/main/java/br/com/segcomp/aes/AES128CTR.java
@@ -0,0 +1,69 @@
+package br.com.segcomp.aes;
+
+import br.com.segcomp.aes.block.Block;
+import br.com.segcomp.aes.enums.KeyLength;
+import br.com.segcomp.aes.key.AES128KeyScheduler;
+import br.com.segcomp.aes.key.Key;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class AES128CTR extends AdvancedEncryptionStandard {
+
+    private final int wordSize = 4;
+
+    private AES128Encryptor encryptor;
+    private AES128Decryptor decryptor;
+
+    private byte[] counter;
+
+    public AES128CTR() throws Exception {
+        this.keyLength = KeyLength.KEY128;
+        this.key = new Key(keyLength.getLengthInBytes());
+        this.expandedKey = new AES128KeyScheduler().schedule(key);
+        this.encryptor = new AES128Encryptor();
+        this.decryptor = new AES128Decryptor();
+    }
+
+    @Override
+    void encrypt(Block block) {
+        encryptor.preRound(block, expandedKey[0]);
+        for(int i = 1; i < 10; i++){
+            encryptor.round(block, expandedKey[i]);
+        }
+        encryptor.lastRound(block, expandedKey[10]);
+    }
+
+    @Override
+    void decrypt(Block block) {
+        decryptor.preRound(block, expandedKey[10]);
+        for(int i = 9; i > 0; i--){
+            decryptor.round(block, expandedKey[i]);
+        }
+        decryptor.lastRound(block, expandedKey[0]);
+    }
+
+    void encryptStream(Block[] blocks) {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] counterStart = new byte[16];
+        secureRandom.nextBytes(counterStart);
+        this.counter = counterStart;
+        BigInteger counter = new BigInteger(counterStart);
+        for (Block b : blocks) {
+            Block initializationVector = new Block(counter.toByteArray());
+            encrypt(initializationVector);
+            encryptor.addRoundKey(b, initializationVector);
+            counter = counter.add(BigInteger.ONE);
+        }
+    }
+
+    void decryptStream(Block[] blocks) {
+        BigInteger counter = new BigInteger(this.counter);
+        for (Block b : blocks) {
+            Block initializationVector = new Block(this.counter);
+            decrypt(initializationVector);
+            decryptor.addRoundKey(b, initializationVector);
+            counter = counter.add(BigInteger.ONE);
+        }
+    }
+}
--- src/main/java/br/com/segcomp/aes/App.java
+++ src/main/java/br/com/segcomp/aes/App.java
@@ -5,7 +5,7 @@ import br.com.segcomp.aes.block.Block;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        AdvancedEncryptionStandard aes = new AES128();
+        AdvancedEncryptionStandard aes = new AES128CTR();
         byte[] bytes = {0x00, 0x01, 0x02, 0x02, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
         Block block = new Block(bytes);
         System.out.println(block);
